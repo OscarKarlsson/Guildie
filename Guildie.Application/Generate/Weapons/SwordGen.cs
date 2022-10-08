ff6804e06990f42008671e94cc2b1803bc189a12
OscarKarlsson <70796269+OscarKarlsson@users.noreply.github.com>
2022-10-08 08:49:48 +0200

    SwordGen Start

---
+++ Guildie.Application/Generate/Weapons/SwordGen.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guildie.Application.Generate.Weapons
+{
+    internal enum RarityCommonSword
+    {
+        Sword_of_destruction,
+        Sword_of_might,
+        Sword_of_speed,
+        Dull_rapier,
+        Sword_of_patience,
+        Rusty_katana,
+        Broken_Sabre,
+        Claymore
+    }
+
+    internal enum RarityUncommonSword
+    {
+        Sword_of_mayhem,
+        Sword_of_life,
+        Sword_of_destruction,
+        Sword_of_patience,
+        Katana,
+        Sabre,
+        Rapier,
+        Claymore_of_power
+    }
+
+    internal enum RarityRareSword
+    {
+        Sword_of_insight,
+        Sword_of_power,
+        Sword_of_devastation,
+        Sword_of_patience,
+        Katana_of_the_cheeta,
+        Sabre_of_the_lion,
+        Rapier_of_the_tiger,
+        Claymore_of_immense_power
+    }
+
+    internal enum RarityMythicSword
+    {
+        Gizzerz_sword,
+        Fire_sword,
+        Ascalon,
+        Cheng_Ying,
+        Muramasa
+    }
+
+    internal class SwordGen
+    {
+    }
+}
